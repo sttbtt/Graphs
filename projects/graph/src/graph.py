99d03ef9b5fba730d216573cdf468a2e425ebbab
Scott Bennett <stt.btt@gmail.com>
2019-03-04 18:27:38 -0500

    Initial Commit

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,107 @@
 """
 Simple graph implementation
 """
+class Queue():
+    def __init__(self):
+        self.queue = []
+    
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return (len(self.queue))
+
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return (len(self.stack))
 
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+    
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist")
+    
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            raise IndexError("That vertex does not exist")
+
+    def bft(self, starting_vertex_id):
+        # Create an empty queue
+        q = Queue()
+        # Create an empty set of visited vertices
+        visited = set()
+        # Put the starting vertex in our Queue
+        q.enqueue(starting_vertex_id)
+        # While the queue is not empty
+        while q.size() > 0:
+            # Dequeue the first node from the queue
+            v = q.dequeue()
+            # If that node has not been visited...
+            if v not in visited:
+                # Mark it as visited
+                print(v)
+                visited.add(v)
+                # Then, put all of it's children into the queue
+                for neighbor in self.vertices[v]:
+                    q.enqueue(neighbor)
+
+    def dft(self, starting_vertex_id):
+        # Create an empty queue
+        s = Stack()
+        # Create an empty set of visited vertices
+        visited = set()
+        # Put the starting vertex in our Queue
+        s.push(starting_vertex_id)
+        # While the queue is not empty
+        while s.size() > 0:
+            # Dequeue the first node from the queue
+            v = s.pop()
+            # If that node has not been visited...
+            if v not in visited:
+                # Mark it as visited
+                print(v)
+                visited.add(v)
+                # Then, put all of it's children into the queue
+                for neighbor in self.vertices[v]:
+                    s.push(neighbor)
+    
+    def dft_recursion(self, starting_vertex_id, path = []):
+        path.append(starting_vertex_id)
+
+        for neighbor in self.vertices:
+            if neighbor not in path:
+                path = self.dft_recursion(neighbor, path)
+        
+        return path
+
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -28,7 +28,24 @@ def main():
     graph.add_directed_edge('3', '5')
     graph.add_directed_edge('2', '3')
     graph.add_directed_edge('4', '6')
-    print(graph.vertices)
+ 
+    # graph.add_directed_edge('1', '2')
+    # graph.add_directed_edge('1', '3')
+    # graph.add_directed_edge('2', '4')
+    # graph.add_directed_edge('2', '5')
+    # graph.add_directed_edge('3', '5')
+    # graph.add_directed_edge('4', '6')
+    # graph.add_directed_edge('5', '6')
+    # graph.add_directed_edge('6', '7')
+    
+    
+    print(f'Graph Vertices: {graph.vertices}')
+    print('Breath-First Traversal:')
+    graph.bft('1')
+    print('Depth-First Traversal:')
+    graph.dft('1')
+    print('Depth-First Recursion:')
+    print(graph.dft_recursion('1'))
 
 if __name__ == '__main__':
     # TODO - parse argv
